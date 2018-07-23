777ec7eed47d2e8fa0101a81bdc28582054e58a0
ak1103dev <ak.apichan1103@gmail.com>
2018-07-23 23:35:27 +0700

    :art: new structure

---
+++ data/resolvers.js
@@ -0,0 +1,11 @@
+const { books } = require('./mocks');
+
+// Resolvers define the technique for fetching the types in the
+// schema.  We'll retrieve books from the "books" array above.
+const resolvers = {
+  Query: {
+    books: () => books,
+  },
+};
+
+module.exports = resolvers;
